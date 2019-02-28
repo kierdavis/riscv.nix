711a6f47e5d8d77b414a6991e1fb413925e7f7b0
Kier Davis <kierdavis@gmail.com>
2019-02-28 16:51:23 +0000

    riscv-gnu-toolchain: use deterministic source for newlib

--- riscv-gnu-toolchain.nix
+++ riscv-gnu-toolchain.nix
@@ -2,6 +2,20 @@
 
 let
   nixpkgs = import <nixpkgs> {};
+
+  newlib = rec {
+    version = "2.2.0";
+    src = nixpkgs.fetchurl {
+      url = "ftp://sourceware.org/pub/newlib/newlib-${version}.tar.gz";
+      sha256 = "1gimncxzq663l4gp8zd89ynfzhk2q802mcaiyjpr2xbkn1ix5bgq";
+    };
+  };
+
+  distdir = nixpkgs.runCommand "riscv-gnu-toolchain-distdir" {} ''
+    mkdir -p $out
+    ln -s ${newlib.src} $out/newlib-${newlib.version}.tar.gz
+  '';
+
 in
   stdenv.mkDerivation rec {
     name = "riscv-gnu-toolchain";
@@ -12,4 +26,5 @@ in
       url = "git://github.com/riscv/riscv-gnu-toolchain.git";
       sha256 = "16w4nn6gni5n5i2dbb31w4fw2mbahmg4j199drpvpl6mzwydws7j";
     };
+    buildFlags = ["DISTDIR=${distdir}"];
   }
